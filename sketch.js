e0b08a8bd5392c43ebbfc3d753a56c88e1ab30f7
Lasaskia <00001104262046sp@al.educacao.sp.gov.br>
2024-10-03 17:22:54 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(500, 500);
+  background("green");
+}
+
+function draw() {
+ stroke("red");
+  fill("pink");
+  
+  if(mouseIsPressed){
+  rect(mouseX,mouseY, 20 ,35);
+  }
+}
